246010a6ea525957139fba8317fa898a42bd2412
Curly-kali <bedounia14@gmail.com>
2025-01-07 23:59:00 +0100

    Creation d'un header et de la page Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import Dachbord from './Components/Dashbord/Dachbord';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Dashbord/Header';
+import Home from './Components/Dashbord/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Routes>
+          <Route path='/home' element={<Home />} />
           <Route path="/" element={<Dachbord />} />
         </Routes>
       </div>
---
+++ client/src/Components/Dashbord/NavList.jsx
@@ -0,0 +1,16 @@
+export default function NavList() {
+    return (
+        <ul className="flex space-x-6">
+            <li>
+                <a href="#" className="text-gray-700 font-semibold hover:text-blue-500">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="#" className="text-gray-700 font-semibold hover:text-blue-500">
+                    Dashboard
+                </a>
+            </li>
+        </ul>
+    );
+}
