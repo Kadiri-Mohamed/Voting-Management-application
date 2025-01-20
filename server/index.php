c5829c9d3a28379cbd987a1ca2a8035576ab1316
Nouhaila-Nachat <nouhailanachat23@gmail.com>
2025-01-20 22:04:35 +0100

    les nouvelles taches

--- server/index.php
+++ server/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once '../controllers/UserController.php'; 
+require_once '../controllers/PollsController.php'; 
+
 
 $action = $_GET['action'] ?? ''; 
 $instance = new UserController(); 
+$pool_instance = new PollsController();
 
 switch ($action) {
     case 'signup':
@@ -12,7 +15,13 @@ switch ($action) {
     case 'login':
         $instance->logIn(); 
         break;
-
+    case 'getPollsByUserId':
+        $pool_instance->getPollsByUserId();
+        break;
+    case 'getPublicPolls' :
+        $pool_instance->getPublicPolls();
+        break;
+         
     default:
     echo json_encode([
        
